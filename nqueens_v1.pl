bfe31f4e42b2f556c38cd746dbb9d3edc1ca8523
PanPapag <pan_papageorgiou@hotmail.com>
2019-04-07 22:47:37 +0300

    Version1

---
+++ nqueens_v1.pl
@@ -0,0 +1,30 @@
+:- lib(ic).
+
+nqueens(N, Solution) :-
+  length(Solution, N),
+  Solution #:: 1..N,
+  constrain(Solution),
+  generate(Solution).
+
+constrain([]).
+constrain([X|Xs]) :-
+  noattack(X, Xs, 1),
+  constrain(Xs).
+
+noattack(_, [], _).
+noattack(X, [Y|Ys], M) :-
+  X #\= Y,
+  X #\= Y - M,
+  X #\= Y + M,
+  M1 is M + 1,
+  noattack(X, Ys, M1).
+
+generate(Sol) :-
+  search().
+
+go(N) :-
+  cputime(T1),
+  findall(Sol, nqueens(N, Sol), Sols),
+  cputime(T2),
+  length(Sols, NSols),
+  Time is T2 - T1.
