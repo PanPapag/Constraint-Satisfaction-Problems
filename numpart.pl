f60bd21c25316688a43d8817649cfb1a40625095
PanPapag <pan_papageorgiou@hotmail.com>
2019-04-09 02:05:52 +0300

    trying opt

--- numpart.pl
+++ numpart.pl
@@ -1,22 +1,17 @@
-:- lib(fd).
+:- lib(ic).
 
 numpart(N, SubSet1, SubSet2) :-
   length(Set, N),
-  Set :: 1..N,
+  Set #:: 1..N,
   alldifferent(Set),
   append(SubSet1, SubSet2, Set),
   ordered(SubSet1), ordered(SubSet2),
-  length(SubSet1, K), length(SubSet2, K),
+  length(SubSet1, L1), length(SubSet2, L2), eval(L1) #= eval(L2),
   SubSet1 = [First|_], First #= 1,
-  get_sum(SubSet1, S1), get_sum(SubSet2, S2), S1 #= S2,
-  get_square_sum(SubSet1, SQ1), get_square_sum(SubSet2, SQ2), SQ1 #= SQ2,
-  generate(Set).
-
-generate([]).
-generate(Elements) :-
-  deleteffc(Element, Elements, RestElements),
-  indomain(Element),
-  generate(RestElements).
+  get_sum(SubSet1, S1), get_sum(SubSet2, S2),
+  get_square_sum(SubSet1, SQ1), get_square_sum(SubSet2, SQ2),
+  eval(S1) #= eval(S2), eval(SQ1) #= eval(SQ2),
+  search(Set, 0, input_order, indomain_min, complete, []).
 
 get_sum([], 0).
 get_sum([H|T], Sum + H) :-
