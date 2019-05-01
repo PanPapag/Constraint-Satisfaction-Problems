118dec27074d7d0f00de0d062a85a7132e17aab9
PanPapag <pan_papageorgiou@hotmail.com>
2019-05-01 16:27:28 +0300

    nothing yet

--- MaxSat/maxsat.pl
+++ MaxSat/maxsat.pl
@@ -1,3 +1,10 @@
+:- lib(ic).
+:- lib(branch_and_bound).
+
 maxsat(NV, NC, D, F, S, M) :-
-  create_formula(NV, NC, D, F).
-  
+  create_formula(NV, NC, D, F),
+  def_vars(S, NV).
+
+def_vars(S ,NV) :-
+  length(S, NV),
+  S #:: 0..1.
