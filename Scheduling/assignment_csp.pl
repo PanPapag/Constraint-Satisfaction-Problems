380bc519bd2fe4a4accb66529438634284a535eb
PanPapag <pan_papageorgiou@hotmail.com>
2019-05-21 18:18:02 +0300

    In the right way to win CLP

--- Scheduling/assignment_csp.pl
+++ Scheduling/assignment_csp.pl
@@ -1,19 +1,26 @@
 :- lib(ic).
-:- lib(swi).
+:- lib(listut).
 :- [small_activity].
 %:- [big_activity].
 
-assignment_csp(NP, ST, ASP, ASA) :-
-  findall(activity(A,act(S,E)), activity(A,act(S,E)), Activities),
-  length(Activities, NA),
-  def_var(Solution, NP, NA),
-  constrain(Activities, Solution),
+assignment_csp(NP, ST, ASP, Solution) :-
+  findall(activity(A,act(S,E)), activity(A,act(S,E)), Activities),               % extract all activities into a list
+  length(Activities, NA),                                                        % get number of activities
+  init_TS(NP, LTS),                                                              % at start each person has a time stamp equals -1
+  def_var(Solution, NP, NA), writeln(Activities),
+  %constrain(Activities, LTS, Solution),
   search(Solution, 0, first_fail, indomain, complete, []).
 
-
-constrain([], []).
-
+init_TS(0, []).
+init_TS(RP, [-1|RTS]) :-
+  NRP is RP - 1,
+  init_TS(NRP, RTS).
 
 def_var(S, NP, NA) :-
   length(S, NA),
   S #:: 1..NP.
+
+constrain([], _, []).
+constrain([activity(A,act(S,E))|RestActivities], LTS, [P|RP]) :-
+
+  constrain(RestActivities, NLTS, RP).
