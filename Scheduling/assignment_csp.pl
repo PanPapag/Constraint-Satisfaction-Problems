9bc3a722dbb828f21b75328a8880576c37f62f60
PanPapag <pan_papageorgiou@hotmail.com>
2019-06-10 19:20:04 +0300

    Lets finish this shit

--- Scheduling/assignment_csp.pl
+++ Scheduling/assignment_csp.pl
@@ -1,52 +1,69 @@
 :- lib(ic).
-:- lib(listut).
 :- [small_activity].
 %:- [big_activity].
 
 assignment_csp(NP, ST, ASP, ASA) :-
   findall(activity(A,act(S,E)), activity(A,act(S,E)), Activities),
-  length(Activities, NA),
-  init_PTS(NP, PTS), init_PTT(NP, PTT), !,
-  def_var(Solution, NP, NA),
-  constrain(ST, PTS, PTT, Activities, Solution),
-  search(Solution, 0, first_fail, indomain, complete, []),
-  construct_ASA(Activities, Solution, ASA).
-  %construct_ASP(NP, ASA, RevASP), reverse(RevASP, ASP).
+  findall(X, between(1, NP, X), Persons),
+  length(Activities, NoActs),
+  def_vars(Solution, NoActs, NP),
+  make_tmpl(Activities, Solution, Assigments),
+  %assignment_aux(NoActs, NP, ST, Assigments),
+  construct_ASA(Assigments, [], ASA), nl.
+  %construct_ASP(Persons, Assigments, [], ASP), nl.
+  %search(Solution, 0, first_fail, indomain, complete, []).
 
-% init time stamp for each person to -1
-init_PTS(0, []).
-init_PTS(Person, [-1|RTS]) :-
-  NextPerson is Person - 1,
-  init_PTS(NextPerson, RTS).
+def_vars(Solution, NoActs, NP) :-
+  length(Solution, NoActs),
+  Solution #:: 1..NP.
 
-% init total time for each person to 0
-init_PTT(0, []).
-init_PTT(Person, [0|RTT]) :-
-  NextPerson is Person - 1,
-  init_PTT(NextPerson, RTT).
+assignment_aux(_, _, _, []).
+assignment_aux(Position, NP, ST, [assigned(activity(_,act(S,E)),Person)|Others]) :-
+  NewPosition is Position - 1,
+  assignment_aux(NewPosition, NP, ST, Others),
+  fix(Position, NP, Person),
+  available(ST, S, E, Others, Person).
 
-% define a list of length NA (no. of Activities) which takes value between 1 and NP
-def_var(S, NP, NA) :-
-  length(S, NA),
-  S #:: 1..NP.
+fix(Position, NP, Position) :- Position < NP.
+fix(Position, NP, _) :- Position >= NP.
 
-constrain(_, _, _, [], []).
-constrain(ST, PTS, PTT, [activity(_,act(S,E))|RestActivities], [P|RP]) :-
-  nth1(P, PTS, TS), S - TS #>= 1, replace(PTS, P, E, NPTS),
-  nth1(P, PTT, TT), CTT is TT + E - S, CTT #=< ST, replace(PTT, P, CTT, NPTT),
-  constrain(ST, NPTS, NPTT, RestActivities, RP).
+available(ST, S, E, Assignments, Person) :-
+  findall(activity(PA,act(PS,PE)),member(assigned(activity(PA,act(PS,PE)),Person),Assignments), PersonActivities),
+  check_time_confictions(S, E, PersonActivities),
+  calculate_total_time(PersonActivities, CT),
+  CT + E - S =< ST.
 
-construct_ASA([], [], []).
-construct_ASA([activity(A,act(_,_))|RestActivities], [P|RP], [A - P|RestASA]) :-
-  construct_ASA(RestActivities, RP, RestASA).
+check_time_confictions(_, _, []).
+check_time_confictions(S, E, [activity(_,act(_,PE))|RestOfPersonActivities]) :-
+  S - PE >= 1,
+  check_time_confictions(S, E, RestOfPersonActivities).
+check_time_confictions(S, E, [activity(_,act(PS,_))|RestOfPersonActivities]) :-
+  PS - E >= 1,
+  check_time_confictions(S, E, RestOfPersonActivities).
 
-construct_ASP(0, _, []).
-construct_ASP(Person, ASA, [Person - PA|RestASP]) :-
-  findall(A, member((A - Person), ASA), PA),
-  NextPerson is Person - 1,
-  construct_ASP(NextPerson, ASA, RestASP).
+calculate_total_time([], 0).
+calculate_total_time([activity(_,act(PS,PE))|RestOfPersonActivities], CT) :-
+  calculate_total_time(RestOfPersonActivities, RCT),
+  CT is PE - PS + RCT.
 
-replace([_|T], 1, X,[X|T]).
-replace([H|T], I, X, [H|R]) :-
-  I1 is I - 1,
-  replace(T, I1, X, R).
+make_tmpl([], [], []).
+make_tmpl([activity(A,act(S,E))|RestOfActivities],[Person|RestPeople], [assigned(activity(A,act(S,E)), Person)|Partial]) :-
+  make_tmpl(RestOfActivities, RestPeople, Partial).
+
+construct_ASA([], Partial, ASA) :- reverse(Partial,ASA).
+construct_ASA([assigned(activity(A,act(_,_)),Person)|RestOfAssigments], Partial, ASA) :-
+  construct_ASA(RestOfAssigments, [A - Person|Partial], ASA).
+
+construct_ASP([], _, Partial, ASP) :- reverse(Partial,ASP).
+construct_ASP([Person|RestOfPersons], Assigments, Partial, ASP) :-
+  findall(PA,member(assigned(activity(PA,act(PS,PE)),Person),Assigments), OnlyPersonActivities),
+  findall(activity(PA,act(PS,PE)), member(assigned(activity(PA,act(PS,PE)),Person),Assigments), PersonActivities),
+  calculate_total_time(PersonActivities, TT),
+  construct_ASP(RestOfPersons, Assigments, [Person - OnlyPersonActivities - TT|Partial], ASP).
+
+
+between(I, J, I) :- I =< J.
+between(I, J, X) :-
+   I < J,
+   I1 is I+1,
+   between(I1, J, X).
