701fc0a0187b44240e2d7f8192bcc2280c0ad7f7
PanPapag <pan_papageorgiou@hotmail.com>
2019-06-13 21:35:14 +0300

    Doing costlist

--- Scheduling/assignment_csp.pl
+++ Scheduling/assignment_csp.pl
@@ -9,6 +9,7 @@ assignment_csp(NP, ST, ASP, ASA) :-
   make_tmpl(Activities, Solution, Assignments),
   assignment_aux(NoActs, NP, ST, Assignments),
   construct_ASA(Assignments, [], ASA), nl,
+  compute_A(NP, Activities, A),
   search(Solution, 0, first_fail, indomain, complete, []).
 
 def_vars(Solution, NoActs, NP) :-
@@ -18,10 +19,6 @@ def_vars(Solution, NoActs, NP) :-
 make_tmpl([], [], []).
 make_tmpl([activity(A,act(S,E))|RestOfActivities],[Person|RestPeople], [assigned(activity(A,act(S,E)),Person)|Partial]) :-
   make_tmpl(RestOfActivities, RestPeople, Partial).
-
-construct_ASA([], Partial, ASA) :- reverse(Partial,ASA).
-construct_ASA([assigned(activity(A,act(_,_)),Person)|RestOfAssignments], Partial, ASA) :-
-  construct_ASA(RestOfAssignments, [A - Person|Partial], ASA).
 
 assignment_aux(_, _, _, []).
 assignment_aux(Position, NP, ST, [assigned(activity(_,act(S,E)),Person)|Others]) :-
@@ -51,3 +48,19 @@ constrain(Person, S, E, CST, ST,[assigned(activity(_,act(S1,E1)),Selected)|Other
   Duration is E1 - S1, E1 < S,
   CST1 is CST + Duration, CST1 =< ST,
   constrain(Person, S, E, CST1, ST, Others).
+
+construct_ASA([], Partial, ASA) :- reverse(Partial,ASA).
+construct_ASA([assigned(activity(A,act(_,_)),Person)|RestOfAssignments], Partial, ASA) :-
+  construct_ASA(RestOfAssignments, [A - Person|Partial], ASA).
+
+
+compute_A(NP, Activities, A) :-
+  compute_D(Activities, 0, D),
+  D_A is D / NP,
+  A is round(D_A).
+
+compute_D([], D, D).
+compute_D([activity(_,act(S,E))|Res], C, D) :-
+  Duration is E - S,
+  C1 is C + Duration,
+  compute_D(Res, C1, D).
